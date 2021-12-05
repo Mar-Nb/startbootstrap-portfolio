d79160987a170022b502801a72e0a417c0222b4f
Mar-Nb <niombela.martin@gmail.com>
2021-12-05 21:31:43 +0100

    Ajout des images des projets

--- js/scripts.js
+++ js/scripts.js
@@ -67,4 +67,18 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     }
 
+    // Activate the appearance of all the popover of the page
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
+    });
+
+    var popover = new bootstrap.Popover(document.querySelector('#discordPop'), {
+        container: 'body'
+    });
+
+    var popover = new bootstrap.Popover(document.querySelector('#discordPop'), {
+        trigger: 'focus'
+    });
+
 });
